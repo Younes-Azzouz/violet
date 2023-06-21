125dddc42a3b026eebadfd200080e7ae141fc725
AdnanHamdoun <119082408+AdnanHamdoun@users.noreply.github.com>
2023-06-21 11:27:15 +0200

    updated fresh start

--- assi2/fox.py
+++ assi2/fox.py
@@ -1,7 +1,16 @@
-from vi import Agent, Simulation
+from vi import Agent, Simulation, Config
+
+class Rabbit(Agent):
+    pass
+
+class Fox(Agent):
+    pass
 
 (
-    Simulation()
-    .batch_spawn_agents(100, Agent, images=['images/cockroach.png'])
+    Simulation(Config(
+        image_rotation = True
+    ))
+    .batch_spawn_agents(100, Rabbit, images=['images/cockroach.png'])
+    .batch_spawn_agents(100, Fox, images=['images/bird.png'])
     .run()
 )

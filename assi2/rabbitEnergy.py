154d504588376a607f1a873a464b606aabb9284d
AdnanHamdoun <119082408+AdnanHamdoun@users.noreply.github.com>
2023-06-21 13:20:06 +0200

    rabbit energy update
    added energy bar, still need to work on grass change image index

--- assi2/rabbitEnergy.py
+++ assi2/rabbitEnergy.py
@@ -1,5 +1,7 @@
 from vi import Agent, Simulation, Config
 import random
+import datetime
+import time
 
 
 class Grass(Agent):
@@ -7,19 +9,29 @@ class Grass(Agent):
         # Static grass
         self.freeze_movement()
         
-
+    
 class Rabbit(Agent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.energy_bar = 60  
+
+
     def update(self):
         # Random reproduction of rabbits
         if random.random() < 0.003:
             self.reproduce()
 
         # Rabbit eating grass -> replenish energy
+        self.energy_bar -= .04
+        if self.energy_bar <= 0:
+            self.kill()
         in_proximity = self.in_proximity_accuracy().filter_kind(Grass)
         for agent, _ in in_proximity:
             if agent.alive():
                 agent.kill()
-            # Add here the energy function from fox
+                agent.change_image(0)
+                self.energy_bar = 60
+                
     
 
 class Fox(Agent):
@@ -42,6 +54,7 @@ class Fox(Agent):
     ))
     .batch_spawn_agents(10, Rabbit, images=['assi2/images/red.png'])
     .batch_spawn_agents(10, Fox, images=['assi2/images/bird.png'])
-    .batch_spawn_agents(5, Grass, images=['assi2/images/green.png'])
+    .batch_spawn_agents(5, Grass, images=['assi2/images/green.png'
+                                          ,'assi2/images/white.png'])
     .run()
 )

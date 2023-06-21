2e950ca3eb64b50c19da193805aa1304fb539a4a
MSI <mohammarzy@gmail.com>
2023-06-21 12:57:36 +0200

    energy bar

--- assi2/mohammad.py
+++ assi2/mohammad.py
@@ -5,19 +5,31 @@ class Rabbit(Agent):
     def update(self):
         if random.random() < 0.003:
             self.reproduce()
-                
 
 class Fox(Agent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.energy_bar = 60  
+
     def update(self):
+        self.energy_bar -= .04
+        print(self.energy_bar)
+
+        if self.energy_bar <= 0:
+            self.kill()
+            return
+
         if random.random() < 0.002:
             self.kill()
             return
 
         in_proximity = self.in_proximity_accuracy().filter_kind(Rabbit)
         for agent, _ in in_proximity:
-            if agent.alive():
+            if agent.alive(): 
                 agent.kill()
                 self.reproduce()
+                self.energy_bar = 60  
+                break  
 
 (
     Simulation(Config(image_rotation=True))
